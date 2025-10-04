fd48abedca33c6df5c6fd660976417e0c0a5db14
kimura114514 <332933449@QQ.COM>
2025-10-05 01:57:16 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,73 @@
 import Image from "next/image";
 
-export default function Home() {
+
+const people = [
+  {
+    id: 1,
+    name: '凯瑟琳·约翰逊',
+    profession: '数学家',
+    accomplishment: '太空飞行相关数值的核算',
+    imageId: 'MK3eW3A',
+  },
+  {
+    id: 5,
+    name: '马里奥·莫利纳',
+    profession: '化学家',
+    accomplishment: '北极臭氧空洞的发现',
+    imageId: 'mynHUSa',
+  },
+  {
+    id: 2,
+    name: '穆罕默德·阿卜杜勒·萨拉姆',
+    profession: '物理学家',
+    accomplishment: '关于基本粒子间弱相互作用和电磁相互作用的统一理论',
+    imageId: 'bE7W1ji',
+  },
+  {
+    id: 3,
+    name: '珀西·莱温·朱利亚',
+    profession: '化学家',
+    accomplishment: '开创性的可的松药物、类固醇和避孕药的研究',
+    imageId: 'IOjWm71',
+  },
+  {
+    id: 4,
+    name: '苏布拉马尼扬·钱德拉塞卡',
+    profession: '天体物理学家',
+    accomplishment: '白矮星质量计算',
+    imageId: 'lrWQx8l',
+  },
+];
+
+function Item({ name='', isPacked =true}) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <li className="item">
+      {isPacked ? name + ' ✅' : <del> {name +' ❌'}</del>}
+    </li>
+  );
+
+}
+
+export default function Home() {
+  const chemists = people.filter(person =>
+    person.profession != ''
+  );
+  const listItems = chemists.map(person =>
+    <li key={person.id}>
+            <img
+        src={'https://i.imgur.com/' + person.imageId +'s.jpg'}
+        alt={person.name}
+      />
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        因{person.accomplishment}而闻名世界
+    </li>
+  );
+
+  return (
+    <div>
+    {/*<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,11 +76,11 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              app/page.tsx
             </code>
             .
           </li>
@@ -27,6 +91,21 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -35,8 +114,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </main> */}
+      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -82,7 +161,25 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
+      <section>
+      <h1>Sally Ride 的行李清单</h1>
+      <ul>
+        <Item 
+          isPacked={true} 
+          name="宇航服" 
+        />
+        <Item 
+          isPacked={true} 
+          name="带金箔的头盔" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Tam 的照片" 
+        />
+      </ul>
+      <ul>{listItems}</ul>
+      </section>
     </div>
   );
 }
